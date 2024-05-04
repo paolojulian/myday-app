c1471cd84efa7fc005d75bf76f8c6c833ff1aec6
Paolo Vincent Julian <paolo.julian@accelo.com>
2024-05-04 13:19:10 +0800

    Add bottom tab navigation with custom UI

--- src/navigation/BottomTabNavigator.tsx
+++ src/navigation/BottomTabNavigator.tsx
@@ -3,17 +3,69 @@ import HomeScreen from '../screens/HomeScreen';
 import CalendarScreen from '../screens/CalendarScreen';
 import ExpenseScreen from '../screens/ExpenseScreen';
 import TodoScreen from '../screens/TodoScreen';
+import { Text, TouchableOpacity, View } from 'react-native';
 
 const Tab = createBottomTabNavigator();
 
+enum TabName {
+  Home = 'Home',
+  Calendar = 'Calendar',
+  Add = 'Add',
+  Expense = 'Expense',
+  Todo = 'Todo',
+}
+
 export default function BottomTabNavigator() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name='Home' component={HomeScreen}></Tab.Screen>
-      <Tab.Screen name='Calendar' component={CalendarScreen}></Tab.Screen>
-      <Tab.Screen name='Add' component={() => null}></Tab.Screen>
-      <Tab.Screen name='Expense' component={ExpenseScreen}></Tab.Screen>
-      <Tab.Screen name='Todo' component={TodoScreen}></Tab.Screen>
+    <Tab.Navigator
+      tabBar={(props) => (
+        <View style={{ flexDirection: 'row' }}>
+          <TouchableOpacity
+            onPress={() => props.navigation.navigate(TabName.Home)}
+          >
+            <Text>Home</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => props.navigation.navigate(TabName.Calendar)}
+          >
+            <Text>Calendar</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => props.navigation.navigate('Add')}
+            style={{
+              position: 'absolute',
+              bottom: 20, // space from bottombar
+              backgroundColor: 'blue',
+              width: 70,
+              height: 70,
+              borderRadius: 35,
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}
+          >
+            <Text>Add</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => props.navigation.navigate(TabName.Expense)}
+          >
+            <Text>Expense</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => props.navigation.navigate(TabName.Todo)}
+          >
+            <Text>Todo</Text>
+          </TouchableOpacity>
+        </View>
+      )}
+    >
+      <Tab.Screen name={TabName.Home} component={HomeScreen}></Tab.Screen>
+      <Tab.Screen
+        name={TabName.Calendar}
+        component={CalendarScreen}
+      ></Tab.Screen>
+      <Tab.Screen name={TabName.Add} component={() => null}></Tab.Screen>
+      <Tab.Screen name={TabName.Expense} component={ExpenseScreen}></Tab.Screen>
+      <Tab.Screen name={TabName.Todo} component={TodoScreen}></Tab.Screen>
     </Tab.Navigator>
   );
 }
