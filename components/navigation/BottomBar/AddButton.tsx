cb7bac68ddc3f5b9166933e174119804f71fc151
Paolo Vincent Julian <paolo.julian@accelo.com>
2024-06-01 11:58:38 +0800

    style // finalize add button style

--- components/navigation/BottomBar/AddButton.tsx
+++ components/navigation/BottomBar/AddButton.tsx
@@ -1,6 +1,6 @@
 import { colors } from '@/constants/Colors';
 import { Ionicons } from '@expo/vector-icons';
-import { TouchableOpacity } from 'react-native';
+import { TouchableOpacity, View } from 'react-native';
 
 export type AddButtonProps = {
   onPress: () => void;
@@ -14,15 +14,27 @@ export default function AddButton({ onPress: onAddPress }: AddButtonProps) {
         onAddPress();
       }}
       style={{
-        backgroundColor: colors.black,
+        backgroundColor: colors.grey,
         width: 64,
         height: 64,
+        padding: 8,
         borderRadius: 9999,
         justifyContent: 'center',
         alignItems: 'center',
       }}
     >
-      <Ionicons name="add-outline" color={colors.white} size={32} />
+      <View
+        style={{
+          backgroundColor: colors.black,
+          width: 48,
+          height: 48,
+          borderRadius: 9999,
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <Ionicons name="add-outline" color={colors.white} size={32} />
+      </View>
     </TouchableOpacity>
   );
 }
