e70567d85726e46cc168f0d7c66aa1d16a388b7c
Kei Hiragi <khiragi0716@gmail.com>
2024-06-02 13:20:27 +0900

    useMutation

--- hooks/services/useMutation.tsx
+++ hooks/services/useMutation.tsx
@@ -1,27 +1,24 @@
-import { useState, useEffect } from "react";
+import { useState } from 'react';
 
 export function useMutation<T>(callback: () => Promise<T>) {
-    const [data, setData] = useState<T>();
-    const [isLoading, setIsLoading] = useState(false);
-    const [error, setErrors] = useState<unknown>();
+  const [data, setData] = useState<T>();
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setErrors] = useState<unknown>();
 
-    async function mutate() {
-        try {
-            setIsLoading(true);
-            const result = await callback();
-            setData(result);
-        } catch (e) {
-            setErrors(e);
-        } finally {
-            setIsLoading(false)
-        }
+  async function mutate() {
+    try {
+      setIsLoading(true);
+      const result = await callback();
+      setData(result);
+      return result;
+    } catch (e) {
+      setErrors(e);
+    } finally {
+      setIsLoading(false);
     }
+  }
 
-    useEffect(() => {
-        mutate()
-    }, []);
-
-    return { isLoading, error, data, mutate }
-};
+  return { isLoading, error, data, mutate };
+}
 
 export default useMutation;
