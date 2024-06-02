34e47dcf7c1d6da3b4a0c19757e5a0304c292e5c
Kei Hiragi <khiragi0716@gmail.com>
2024-06-03 00:07:26 +0900

    fix migrations

--- hooks/services/expenses/useExpense.tsx
+++ hooks/services/expenses/useExpense.tsx
@@ -3,12 +3,12 @@ import useQuery from '../useQuery';
 
 const useExpense = (filters: Filter[]) => {
   const db = useSQLiteContext();
-  const { data, isLoading, error } = useQuery(setup)
+  const { data, isLoading, error } = useQuery(setup);
 
   async function setup() {
     const { whereString, values } = filtersToString(filters);
-    const result = await db.getAllAsync<Expense>('SELECT * FROM Expenses ' + whereString, values)
-    return result
+    const result = await db.getAllAsync<Expense>('SELECT * FROM expense ' + whereString, values);
+    return result;
   }
 
   return { data, isLoading, error };
@@ -16,17 +16,17 @@ const useExpense = (filters: Filter[]) => {
 
 const filtersToString = (filters: Filter[]) => {
   if (filters.length < 1) {
-    return { whereString: '', values: [] }
+    return { whereString: '', values: [] };
   }
 
   let whereString = 'WHERE ';
   let values: string[] = [];
   filters.forEach(element => {
     whereString += element.columnName + ' ' + element.condition + '?';
-    values.push(element.value)
+    values.push(element.value);
   });
 
   return { whereString, values };
-}
+};
 
 export default useExpense;
