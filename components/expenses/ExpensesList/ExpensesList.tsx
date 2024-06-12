8e2bf313c17193f8a9e00c276fa4359b270260ff
Paolo Vincent Julian <paolo.julian@accelo.com>
2024-06-12 16:59:24 +0800

    doc: prepare the delete item function

--- components/expenses/ExpensesList/ExpensesList.tsx
+++ components/expenses/ExpensesList/ExpensesList.tsx
@@ -2,6 +2,7 @@ import Container from '@/components/common/Container';
 import ExpenseItem from './ExpensesListItem/ExpenseItem';
 import useExpense from '@/hooks/services/expense/useExpense';
 import ThemedText from '@/components/common/ThemedText';
+import { Expense } from '@/hooks/services/expense/expense.types';
 
 export default function ExpensesList() {
   const { data: expenses, isLoading } = useExpense();
@@ -11,9 +12,16 @@ export default function ExpensesList() {
     return <ThemedText>Loading...</ThemedText>;
   }
 
+  const handleDeleteItem = (id: Expense['id']) => {
+    // TODO: add delete function
+    console.log('Deleting item with id: ', id);
+  };
+
   return (
     <Container style={{ gap: 8 }}>
-      {expenses?.map(item => <ExpenseItem key={item.id} onDelete={() => {}} expense={item} />)}
+      {expenses?.map(item => (
+        <ExpenseItem key={item.id} onDelete={handleDeleteItem} expense={item} />
+      ))}
     </Container>
   );
 }
