220882282bf185dcea445d1133e9578ba1075ae7
Paolo Vincent Julian <paolo.julian@accelo.com>
2024-06-27 00:03:58 +0800

    spec: update unit test to handle array of recurring expenses

--- providers/DatabaseProvider/utils/runRecurringExpenses.util.test.ts
+++ providers/DatabaseProvider/utils/runRecurringExpenses.util.test.ts
@@ -278,5 +278,72 @@ describe('TESTING RecurringExpenses class', () => {
         expect(mockExecuteAsync).not.toHaveBeenCalled();
       });
     });
+
+    describe('GIVEN multiple recurring expenses', () => {
+      const mockExecuteAsync = jest.fn();
+      const recurringExpense1 = {
+        ...mockRecurringExpense,
+        id: 1,
+        latest_recurred_transaction_date: dayjs(now)
+          .subtract(4, 'months')
+          .subtract(2, 'days')
+          .unix(),
+        transaction_date: dayjs(now).subtract(4, 'months').unix(),
+      } satisfies RecurringExpense;
+      const recurringExpense2 = {
+        ...mockRecurringExpense,
+        id: 2,
+        latest_recurred_transaction_date: null,
+        transaction_date: dayjs(now).subtract(1, 'month').unix(),
+      } satisfies RecurringExpense;
+
+      const mockDb = {
+        getAllAsync: jest.fn(async () => [recurringExpense1, recurringExpense2]),
+        prepareAsync: async () =>
+          ({
+            executeAsync: mockExecuteAsync,
+            finalizeAsync: jest.fn(),
+          }) as any,
+      } as any;
+      beforeEach(async () => {
+        await new RecurringExpenses(mockDb, now).populate();
+      });
+      afterEach(() => {
+        jest.clearAllMocks();
+      });
+      it('THEN it should create 4 recurred expense for recurringExpense1 and 1 recurred expense for recurringExpense2', async () => {
+        expect(mockExecuteAsync).toHaveBeenCalledTimes(5);
+        expect(mockExecuteAsync).toHaveBeenNthCalledWith(
+          1,
+          expect.objectContaining({
+            $transaction_date: dayjs
+              .unix(recurringExpense1.latest_recurred_transaction_date)
+              .startOf('day')
+              .add(1, 'month')
+              .unix(),
+          }),
+        );
+        expect(mockExecuteAsync).toHaveBeenNthCalledWith(
+          2,
+          expect.objectContaining({
+            $transaction_date: dayjs
+              .unix(recurringExpense2.transaction_date)
+              .startOf('day')
+              .add(1, 'month')
+              .unix(),
+          }),
+        );
+        expect(mockExecuteAsync).toHaveBeenNthCalledWith(
+          3,
+          expect.objectContaining({
+            $transaction_date: dayjs
+              .unix(recurringExpense1.latest_recurred_transaction_date)
+              .startOf('day')
+              .add(2, 'months')
+              .unix(),
+          }),
+        );
+      });
+    });
   });
 });
