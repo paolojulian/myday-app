9dcd0a469bebc319ebd08fac7a8d79e202bc6a7c
Paolo Vincent Julian <paolo.julian@accelo.com>
2024-06-26 23:35:52 +0800

    spec: add recurring expenses

--- providers/DatabaseProvider/utils/runRecurringExpenses.util.test.ts
+++ providers/DatabaseProvider/utils/runRecurringExpenses.util.test.ts
@@ -22,8 +22,9 @@ const mockRecurringExpense = {
 
 describe('TESTING RecurringExpenses class', () => {
   describe('GIVEN a monthly recurring expense', () => {
-    describe('WHEN the app has not been opened for 4 months, but there is a recurred expense 5 months ago', () => {
+    describe('WHEN the app has not been opened for 4 months, but there is a recurred expense 4 months and 3 days ago', () => {
       const mockExecuteAsync = jest.fn();
+      const recurringExpense = { ...mockRecurringExpense };
 
       const mockDb = {
         getAllAsync: jest.fn(async () => [mockRecurringExpense]),
@@ -45,22 +46,22 @@ describe('TESTING RecurringExpenses class', () => {
       it('THEN it should create 4 recurred expense', async () => {
         const expectedTransactionDates = [
           dayjs
-            .unix(mockRecurringExpense.latest_recurred_transaction_date)
+            .unix(recurringExpense.latest_recurred_transaction_date)
             .startOf('day')
             .add(1, 'month')
             .unix(),
           dayjs
-            .unix(mockRecurringExpense.latest_recurred_transaction_date)
+            .unix(recurringExpense.latest_recurred_transaction_date)
             .startOf('day')
             .add(2, 'months')
             .unix(),
           dayjs
-            .unix(mockRecurringExpense.latest_recurred_transaction_date)
+            .unix(recurringExpense.latest_recurred_transaction_date)
             .startOf('day')
             .add(3, 'months')
             .unix(),
           dayjs
-            .unix(mockRecurringExpense.latest_recurred_transaction_date)
+            .unix(recurringExpense.latest_recurred_transaction_date)
             .startOf('day')
             .add(4, 'months')
             .unix(),
@@ -70,10 +71,10 @@ describe('TESTING RecurringExpenses class', () => {
         mockExecuteAsync.mock.calls.forEach((callArgs, index) => {
           expect(callArgs).toEqual([
             expect.objectContaining({
-              $amount: mockRecurringExpense.amount,
-              $category_id: mockRecurringExpense.category_id,
-              $description: mockRecurringExpense.description,
-              $title: mockRecurringExpense.title,
+              $amount: recurringExpense.amount,
+              $category_id: recurringExpense.category_id,
+              $description: recurringExpense.description,
+              $title: recurringExpense.title,
               $transaction_date: expectedTransactionDates[index],
             }),
           ]);
@@ -116,6 +117,130 @@ describe('TESTING RecurringExpenses class', () => {
         ];
 
         expect(mockExecuteAsync).toHaveBeenCalledTimes(4);
+        mockExecuteAsync.mock.calls.forEach((callArgs, index) => {
+          expect(callArgs).toEqual([
+            expect.objectContaining({
+              $amount: recurringExpense.amount,
+              $category_id: recurringExpense.category_id,
+              $description: recurringExpense.description,
+              $title: recurringExpense.title,
+              $transaction_date: expectedTransactionDates[index],
+            }),
+          ]);
+        });
+      });
+    });
+
+    describe('WHEN the app has not been opened for 4 months and there is a recurred expense exactly 4 months ago', () => {
+      const now = dayjs();
+      const recurringExpense = {
+        ...mockRecurringExpense,
+      } satisfies RecurringExpense;
+
+      const mockExecuteAsync = jest.fn();
+
+      const mockDb = {
+        getAllAsync: jest.fn(async () => [recurringExpense]),
+        prepareAsync: async () =>
+          ({
+            executeAsync: mockExecuteAsync,
+            finalizeAsync: jest.fn(),
+          }) as any,
+      } as any;
+
+      beforeEach(async () => {
+        await new RecurringExpenses(mockDb, now).populate();
+      });
+
+      afterEach(() => {
+        mockExecuteAsync.mockClear();
+      });
+
+      it('THEN it should create 4 recurred expense', async () => {
+        const expectedTransactionDates = [
+          dayjs
+            .unix(recurringExpense.latest_recurred_transaction_date)
+            .startOf('day')
+            .add(1, 'month')
+            .unix(),
+          dayjs
+            .unix(recurringExpense.latest_recurred_transaction_date)
+            .startOf('day')
+            .add(2, 'months')
+            .unix(),
+          dayjs
+            .unix(recurringExpense.latest_recurred_transaction_date)
+            .startOf('day')
+            .add(3, 'months')
+            .unix(),
+          dayjs
+            .unix(recurringExpense.latest_recurred_transaction_date)
+            .startOf('day')
+            .add(4, 'months')
+            .unix(),
+        ];
+
+        expect(mockExecuteAsync).toHaveBeenCalledTimes(4);
+        mockExecuteAsync.mock.calls.forEach((callArgs, index) => {
+          expect(callArgs).toEqual([
+            expect.objectContaining({
+              $amount: recurringExpense.amount,
+              $category_id: recurringExpense.category_id,
+              $description: recurringExpense.description,
+              $title: recurringExpense.title,
+              $transaction_date: expectedTransactionDates[index],
+            }),
+          ]);
+        });
+      });
+    });
+
+    describe('WHEN the app has not been opened for 4 months and there is a recurred expense 3 and a half months ago', () => {
+      const now = dayjs();
+      const recurringExpense = {
+        ...mockRecurringExpense,
+        latest_recurred_transaction_date: dayjs(now).subtract(4, 'months').add(15, 'days').unix(),
+      } satisfies RecurringExpense;
+
+      const mockExecuteAsync = jest.fn();
+
+      const mockDb = {
+        getAllAsync: jest.fn(async () => [recurringExpense]),
+        prepareAsync: async () =>
+          ({
+            executeAsync: mockExecuteAsync,
+            finalizeAsync: jest.fn(),
+          }) as any,
+      } as any;
+
+      beforeEach(async () => {
+        await new RecurringExpenses(mockDb, now).populate();
+      });
+
+      afterEach(() => {
+        mockExecuteAsync.mockClear();
+      });
+
+      it('THEN it should create 3 recurred expense', async () => {
+        const expectedTransactionDates = [
+          dayjs
+            .unix(recurringExpense.latest_recurred_transaction_date)
+            .startOf('day')
+            .add(1, 'month')
+            .unix(),
+          dayjs
+            .unix(recurringExpense.latest_recurred_transaction_date)
+            .startOf('day')
+            .add(2, 'months')
+            .unix(),
+          dayjs
+            .unix(recurringExpense.latest_recurred_transaction_date)
+            .startOf('day')
+            .add(3, 'months')
+            .unix(),
+        ];
+
+        expect(mockExecuteAsync).toHaveBeenCalledTimes(3);
         mockExecuteAsync.mock.calls.forEach((callArgs, index) => {
           expect(callArgs).toEqual([
             expect.objectContaining({
