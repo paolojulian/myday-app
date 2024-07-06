e553637017cb9c5fc91605358319bc6b9aa1e970
Paolo Vincent Julian <paolo.julian@accelo.com>
2024-07-06 18:57:30 +0800

    feat: add clickable task overview

--- components/home/TasksOverview/TasksOverview.tsx
+++ components/home/TasksOverview/TasksOverview.tsx
@@ -1,16 +1,30 @@
+import { TabName } from '@/app/(tabs)/_layout';
 import ItemValueBentoCard from '@/components/common/BentoCard/ItemValueBentoCard';
 import Container from '@/components/common/Container';
 import useTaskOverview from '@/hooks/services/task/useTaskOverview';
+import { useNavigation } from 'expo-router';
 import React from 'react';
+import { TouchableOpacity } from 'react-native';
 
 function TasksOverview() {
+  const navigation = useNavigation();
   const { data } = useTaskOverview();
+
+  const goToTaskList = () => {
+    navigation.navigate(TabName.Todo as never);
+  };
 
   return (
     <Container style={{ flexDirection: 'row', gap: 8 }}>
-      <ItemValueBentoCard value={data.totalTasks} label="Total Tasks" />
-      <ItemValueBentoCard value={data.dueToday} label="Due Today" />
-      <ItemValueBentoCard value={data.overdue} label="Overdue" />
+      <TouchableOpacity style={{ flex: 1 }} onPress={goToTaskList}>
+        <ItemValueBentoCard value={data.totalTasks} label="Total Tasks" />
+      </TouchableOpacity>
+      <TouchableOpacity style={{ flex: 1 }} onPress={goToTaskList}>
+        <ItemValueBentoCard value={data.dueToday} label="Due Today" />
+      </TouchableOpacity>
+      <TouchableOpacity style={{ flex: 1 }} onPress={goToTaskList}>
+        <ItemValueBentoCard value={data.overdue} label="Overdue" />
+      </TouchableOpacity>
     </Container>
   );
 }
