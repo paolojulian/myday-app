a3d2785baf34ab21ad9d83b9c78ad4db863dcbed
Paolo Vincent <paolojulian.personal@gmail.com>
2024-07-07 14:55:50 +0800

    Feature: dynamic status bar color (#83)

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,8 +1,15 @@
 import HomeWorkArea from '@/components/home/HomeWorkArea';
+import { colors } from '@/constants/Colors';
+import { StatusBar } from 'expo-status-bar';
+import { SafeAreaView } from 'react-native';
 
 export default function Home() {
   return (
     <>
+      {/* ios */}
+      <SafeAreaView style={{ flex: 0, backgroundColor: colors.slateGrey[100] }} />
+      {/* android */}
+      <StatusBar backgroundColor={colors.slateGrey[100]} />
       <HomeWorkArea />
     </>
   );
