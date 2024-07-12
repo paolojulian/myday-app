70ff2c1f1e71ae0ef893fda780273f668d1c52f8
Paolo Vincent Julian <paolo.julian@accelo.com>
2024-07-12 16:58:09 +0800

    feat: open update manager from edit budget card

--- components/expenses/EditBudgetCard/EditBudgetCard.tsx
+++ components/expenses/EditBudgetCard/EditBudgetCard.tsx
@@ -2,21 +2,29 @@ import BentoCard from '@/components/common/BentoCard';
 import EditIcon from '@/components/common/icons/EditIcon';
 import ThemedView from '@/components/common/ThemedView';
 import React from 'react';
+import { TouchableOpacity } from 'react-native';
+import { UpdateBudgetManager } from '../BudgetCard/UpdateBudgetBottomSheet';
 
 function EditBudgetCard() {
+  const handlePress = () => {
+    UpdateBudgetManager.show();
+  };
+
   return (
-    <BentoCard style={{ flex: 0, aspectRatio: '1/1' }}>
-      <ThemedView
-        style={{
-          gap: 16,
-          alignItems: 'center',
-          justifyContent: 'center',
-          flex: 1,
-        }}
-      >
-        <EditIcon />
-      </ThemedView>
-    </BentoCard>
+    <TouchableOpacity style={{ flex: 0 }} onPress={handlePress}>
+      <BentoCard style={{ aspectRatio: '1/1' }}>
+        <ThemedView
+          style={{
+            gap: 16,
+            alignItems: 'center',
+            justifyContent: 'center',
+            flex: 1,
+          }}
+        >
+          <EditIcon />
+        </ThemedView>
+      </BentoCard>
+    </TouchableOpacity>
   );
 }
 
