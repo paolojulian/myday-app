b2a331ad0176ea5ee3638090b2ab4bf653e3f9c3
Paolo Vincent Julian <paolo.julian@accelo.com>
2024-07-20 15:30:29 +0800

    fix: expense list

--- components/expenses/ExpensesList/ExpensesList.tsx
+++ components/expenses/ExpensesList/ExpensesList.tsx
@@ -47,6 +47,7 @@ export default function ExpensesList() {
       data={[{ isFilter: true }, ...filteredData]}
       contentContainerStyle={{
         justifyContent: 'flex-start',
+        flex: 1,
       }}
       keyExtractor={item => {
         if (isFilter(item)) {
