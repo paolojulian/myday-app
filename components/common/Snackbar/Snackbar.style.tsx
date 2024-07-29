127ed50a89da8ab1bb6af30d2e363048c9f58f15
Paolo Vincent Julian <paolo.julian@accelo.com>
2024-07-29 23:35:34 +0800

    refactor: Snackbar

---
+++ components/common/Snackbar/Snackbar.style.tsx
@@ -0,0 +1,151 @@
+import { colors } from '@/constants/Colors';
+import { FC, ReactNode, useEffect, useRef, useState } from 'react';
+import { Animated, TextStyle, TouchableWithoutFeedback, View, ViewStyle } from 'react-native';
+import { SnackbarType } from './Snackbar';
+import ThemedText from '../ThemedText';
+
+type StyledSnacbarContainerProps = {
+  onExitAnimation: () => void;
+  children: ReactNode;
+  isOpen: boolean;
+  duration_ms: number;
+};
+export const StyledSnacbarContainer: FC<StyledSnacbarContainerProps> = ({
+  onExitAnimation,
+  children,
+  isOpen,
+  duration_ms,
+}) => {
+  const [fadeAnim] = useState(new Animated.Value(0));
+  const [translateYAnim] = useState(new Animated.Value(100));
+  const timeoutRef = useRef<NodeJS.Timeout | null>(null);
+
+  const clearAllTimeout = () => {
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current);
+    }
+  };
+
+  const enterAnimation = (): void => {
+    clearAllTimeout();
+    Animated.timing(fadeAnim, {
+      toValue: 1,
+      duration: 200,
+      useNativeDriver: true,
+    }).start();
+    Animated.timing(translateYAnim, {
+      toValue: 0,
+      duration: 200,
+      useNativeDriver: true,
+    }).start();
+  };
+
+  const exitAnimation = (): void => {
+    Animated.timing(fadeAnim, {
+      toValue: 0,
+      duration: 200,
+      useNativeDriver: true,
+    }).start(() => onExitAnimation());
+    Animated.timing(translateYAnim, {
+      toValue: 100,
+      duration: 200,
+      useNativeDriver: true,
+    }).start(() => onExitAnimation());
+  };
+
+  useEffect(() => {
+    if (isOpen) {
+      enterAnimation();
+      timeoutRef.current = setTimeout(() => {
+        exitAnimation();
+      }, duration_ms);
+    } else {
+      exitAnimation();
+    }
+  }, [isOpen, duration_ms]);
+
+  return (
+    <TouchableWithoutFeedback onPress={exitAnimation}>
+      <Animated.View
+        style={{
+          position: 'absolute',
+          bottom: 120,
+          left: 0,
+          right: 0,
+          flex: 1,
+          opacity: fadeAnim,
+          transform: [
+            {
+              translateY: translateYAnim,
+            },
+          ],
+          zIndex: 999,
+          pointerEvents: isOpen ? 'auto' : 'none',
+        }}
+      >
+        {children}
+      </Animated.View>
+    </TouchableWithoutFeedback>
+  );
+};
+
+type StyledSnackbarCardProps = {
+  children: ReactNode;
+  variant: SnackbarType;
+};
+export const StyledSnackbarCard: FC<StyledSnackbarCardProps> = ({ children, variant }) => {
+  return (
+    <View
+      style={[
+        {
+          height: 56,
+          flexDirection: 'row',
+          alignItems: 'center',
+          justifyContent: 'space-between',
+          elevation: 8,
+          shadowColor: colors.black,
+          shadowOffset: { width: 0, height: 2 },
+          shadowOpacity: 0.15,
+          borderRadius: 8,
+          padding: 16,
+          backgroundColor: colors.red,
+          marginHorizontal: 16,
+        },
+        cardVariantStyle[variant] ?? {},
+      ]}
+    >
+      {children}
+    </View>
+  );
+};
+const cardVariantStyle: Record<SnackbarType, ViewStyle> = {
+  error: {
+    backgroundColor: colors.red,
+  },
+  info: {
+    backgroundColor: colors.grey,
+  },
+  success: {
+    backgroundColor: colors.green,
+  },
+};
+
+type StyledSnackbarMessageProps = {
+  message: string | null;
+  variant: SnackbarType;
+};
+export const StyledSnackbarMessage: FC<StyledSnackbarMessageProps> = ({ message, variant }) => {
+  return <ThemedText style={[messageVariantStyle[variant] ?? {}]}>{message}</ThemedText>;
+};
+
+const messageVariantStyle: Record<SnackbarType, TextStyle> = {
+  info: {
+    color: colors.black,
+  },
+  error: {
+    color: colors.white,
+  },
+  success: {
+    color: colors.white,
+  },
+};
--- components/common/Snackbar/Snackbar.tsx
+++ components/common/Snackbar/Snackbar.tsx
@@ -1,7 +1,9 @@
-import ThemedText from '@/components/common/ThemedText';
-import { colors } from '@/constants/Colors';
-import React, { useEffect, useMemo, useRef, useState } from 'react';
-import { Animated, StyleSheet, TouchableWithoutFeedback } from 'react-native';
+import React from 'react';
+import {
+  StyledSnacbarContainer,
+  StyledSnackbarCard,
+  StyledSnackbarMessage,
+} from './Snackbar.style';
 
 export const SNACKBAR = {
   DURATION_SHORT: 2000,
@@ -25,150 +27,14 @@ function Snackbar({
   duration = SNACKBAR.DURATION_SHORT,
   type = 'info',
 }: SnackbarProps) {
-  const [fadeAnim] = useState(new Animated.Value(0));
-  const [translateYAnim] = useState(new Animated.Value(100));
-  const timeoutRef = useRef<NodeJS.Timeout | null>(null);
-
-  const resolvedContainerStyle = useMemo(() => {
-    switch (type) {
-      case 'error':
-        return errorStyles.container;
-      case 'success':
-        return successStyles.container;
-      default:
-        return infoStyles.container;
-    }
-  }, [type]);
-
-  const resolvedTextStyle = useMemo(() => {
-    switch (type) {
-      case 'error':
-        return errorStyles.text;
-      case 'success':
-        return successStyles.text;
-      default:
-        return infoStyles.text;
-    }
-  }, [type]);
-
-  const enterAnimation = () => {
-    clearAllTimeout();
-    Animated.timing(fadeAnim, {
-      toValue: 1,
-      duration: 200,
-      useNativeDriver: true,
-    }).start();
-    Animated.timing(translateYAnim, {
-      toValue: 0,
-      duration: 200,
-      useNativeDriver: true,
-    }).start();
-  };
-
-  const exitAnimation = () => {
-    Animated.timing(fadeAnim, {
-      toValue: 0,
-      duration: 200,
-      useNativeDriver: true,
-    }).start(() => onDismiss());
-    Animated.timing(translateYAnim, {
-      toValue: 100,
-      duration: 200,
-      useNativeDriver: true,
-    }).start(() => onDismiss());
-  };
-
-  const clearAllTimeout = () => {
-    if (timeoutRef.current) {
-      clearTimeout(timeoutRef.current);
-    }
-  };
-
-  useEffect(() => {
-    if (message) {
-      enterAnimation();
-
-      timeoutRef.current = setTimeout(() => {
-        exitAnimation();
-      }, duration);
-    }
-
-    return () => {
-      clearAllTimeout();
-    };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [message, duration]);
-
   return (
-    <TouchableWithoutFeedback onPress={exitAnimation}>
-      <Animated.View
-        style={{
-          position: 'absolute',
-          bottom: 120,
-          left: 0,
-          right: 0,
-          flex: 1,
-          opacity: fadeAnim,
-          transform: [
-            {
-              translateY: translateYAnim,
-            },
-          ],
-          zIndex: 999,
-          pointerEvents: message ? 'auto' : 'none',
-        }}
-      >
-        <Animated.View style={[styles.container, resolvedContainerStyle]}>
-          <ThemedText style={[resolvedTextStyle]}>{message}</ThemedText>
-          {RightComponent}
-        </Animated.View>
-      </Animated.View>
-    </TouchableWithoutFeedback>
+    <StyledSnacbarContainer onExitAnimation={onDismiss} duration_ms={duration} isOpen={!!message}>
+      <StyledSnackbarCard variant={type}>
+        <StyledSnackbarMessage message={message} variant={type} />
+        {RightComponent}
+      </StyledSnackbarCard>
+    </StyledSnacbarContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    height: 56,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    elevation: 8,
-    shadowColor: colors.black,
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.15,
-    borderRadius: 8,
-    padding: 16,
-    backgroundColor: colors.red,
-    marginHorizontal: 16,
-  },
-});
-
-const infoStyles = StyleSheet.create({
-  container: {
-    backgroundColor: colors.grey,
-  },
-  text: {
-    color: colors.black,
-  },
-});
-
-const successStyles = StyleSheet.create({
-  container: {
-    backgroundColor: colors.green,
-  },
-  text: {
-    color: colors.white,
-  },
-});
-
-const errorStyles = StyleSheet.create({
-  container: {
-    backgroundColor: colors.red,
-  },
-  text: {
-    color: colors.white,
-  },
-});
 
 export default Snackbar;
