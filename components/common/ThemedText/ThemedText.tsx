32ca0762b3e0adf90a0743795bf25292fc2c9f7c
Paolo Vincent Julian <paolo.julian@accelo.com>
2024-08-21 22:00:00 +0800

    feat: initialize new typography

--- components/common/ThemedText/ThemedText.tsx
+++ components/common/ThemedText/ThemedText.tsx
@@ -1,13 +1,27 @@
 import { useMemo } from 'react';
-import { StyleSheet, Text, TextProps } from 'react-native';
+import { StyleSheet, Text, TextProps, TextStyle } from 'react-native';
 
+type Variants =
+  | 'heading'
+  | 'body'
+  | 'body1'
+  | 'body2'
+  | 'large'
+  | 'caps'
+  | 'header-sm'
+  | 'header-md'
+  | 'header-lg'
+  | 'body-md'
+  | 'title-sm'
+  | 'title-md'
+  | 'title-lg';
 type ThemedTextProps = {
-  variant?: 'heading' | 'body' | 'body1' | 'body2' | 'large' | 'caps';
+  variant?: Variants;
 } & TextProps;
 
 export default function ThemedText({ variant = 'body', style, ...props }: ThemedTextProps) {
   const variantStyle = useMemo(() => {
-    return themedTextStyles[variant] || themedTextStyles.body;
+    return variantStyles[variant] || variantStyles['body-md'];
   }, [variant]);
 
   return <Text {...props} style={[variantStyle, themedTextStyles.baseStyle, style]}></Text>;
@@ -50,5 +64,76 @@ export const themedTextStyles = StyleSheet.create({
     fontWeight: 500,
     fontFamily: 'PoppinsMedium',
     lineHeight: 24,
+  },
+});
+
+const variantStyles = StyleSheet.create<Record<Variants, TextStyle>>({
+  caps: {
+    fontSize: 64,
+    fontWeight: 800,
+    fontFamily: 'Barlow_CondensedBold',
+    lineHeight: 60,
+  },
+  heading: {
+    fontSize: 24,
+    fontWeight: 600,
+    fontFamily: 'PoppinsSemiBold',
+    lineHeight: 32,
+  },
+  large: {
+    fontSize: 48,
+    fontWeight: 800,
+    fontFamily: 'PoppinsBold',
+    lineHeight: 56,
+  },
+  body: {
+    fontSize: 14,
+    fontFamily: 'PoppinsRegular',
+    lineHeight: 20,
+  },
+  body1: {
+    fontSize: 16,
+    fontFamily: 'PoppinsRegular',
+    lineHeight: 24,
+  },
+  body2: {
+    fontSize: 16,
+    fontWeight: 500,
+    fontFamily: 'PoppinsMedium',
+    lineHeight: 24,
+  },
+  'body-md': {
+    fontSize: 16,
+    fontFamily: 'RalewayRegular',
+  },
+  'header-lg': {
+    fontSize: 28,
+    fontWeight: 700,
+    fontFamily: 'RalewayBold',
+  },
+  'header-md': {
+    fontSize: 20,
+    fontWeight: 700,
+    fontFamily: 'RalewayBold',
+  },
+  'header-sm': {
+    fontSize: 16,
+    fontWeight: 700,
+    fontFamily: 'RalewayBold',
+  },
+  'title-lg': {
+    fontSize: 28,
+    fontWeight: 700,
+    fontFamily: 'RalewayBold',
+  },
+  'title-md': {
+    fontSize: 20,
+    fontWeight: 400,
+    fontFamily: 'RalewayRegular',
+  },
+  'title-sm': {
+    fontSize: 16,
+    fontWeight: 400,
+    fontFamily: 'RalewayRegular',
   },
 });
