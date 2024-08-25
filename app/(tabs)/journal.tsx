93f4bbed59429da872722dcd5274f25aaa32d3d2
Paolo Vincent Julian <paolo.julian@accelo.com>
2024-08-25 08:50:51 +0800

    style: redesign journal

--- app/(tabs)/journal.tsx
+++ app/(tabs)/journal.tsx
@@ -1,12 +1,13 @@
 import ThemedText from '@/components/common/ThemedText';
 import ThemedView from '@/components/common/ThemedView';
+import { colors } from '@/constants/Colors';
 import { useJournal } from '@/hooks/services/journal/useJournal';
 
 export default function JournalScreen() {
   const { data } = useJournal(new Date());
   console.log(data);
   return (
-    <ThemedView>
+    <ThemedView style={{ backgroundColor: colors.v2.black, flex: 1 }}>
       <ThemedText variant="heading">Journal</ThemedText>
     </ThemedView>
   );
