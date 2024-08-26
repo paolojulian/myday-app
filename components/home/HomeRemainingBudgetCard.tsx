80135b2f793c94ade976793d0cb6408e37020fc7
Paolo Vincent Julian <paolo.julian@accelo.com>
2024-08-26 20:41:37 +0800

    home, set budget card

--- components/home/HomeRemainingBudgetCard.tsx
+++ components/home/HomeRemainingBudgetCard.tsx
@@ -4,11 +4,13 @@ import { useTotalExpenses } from '@/hooks/services/expense/useTotalExpenses';
 import { toLocaleCurrencyFormat } from '@/utils/currency/currency.utils';
 import { useFocusEffect } from 'expo-router';
 import React, { useMemo } from 'react';
-import { View } from 'react-native';
+import { TouchableOpacity, View } from 'react-native';
 import AppCard from '../common/AppCard';
 import PieChart from '../common/PieChart';
 import Stack from '../common/Stack';
 import ThemedText from '../common/ThemedText';
+import MoneyIcon from '../common/icons/MoneyIcon';
+import { UpdateBudgetManager } from '../expenses/UpdateBudgetBottomSheet/UpdateBudgetBottomSheet';
 
 function HomeRemainingBudgetCard() {
   const transactionDate = useMemo(() => new Date(), []);
@@ -26,27 +28,50 @@ function HomeRemainingBudgetCard() {
   const monthlyBudget = budget?.amount ?? 0;
   const remainingBudget = monthlyBudget - totalMonthlyExpenses;
 
+  const handleSetBudgetPress = () => {
+    UpdateBudgetManager.show();
+  };
+
   return (
-    <AppCard
-      style={{
-        backgroundColor: colors.v2.teal,
-        height: 197,
-      }}
-    >
-      <Stack style={{ justifyContent: 'center', alignItems: 'center', height: '100%' }}>
-        <View style={{ marginBottom: 16 }}>
-          <PieChart current={remainingBudget} total={monthlyBudget} variant="sm" />
-        </View>
-        <View style={{ alignItems: 'center', gap: 8 }}>
-          <ThemedText variant="header-lg" color="inverted">
-            {toLocaleCurrencyFormat(remainingBudget)}
-          </ThemedText>
-          <ThemedText variant="title-sm" color="inverted">
-            Remaining Budget
-          </ThemedText>
-        </View>
-      </Stack>
-    </AppCard>
+    <TouchableOpacity disabled={!!budget} onPress={handleSetBudgetPress} activeOpacity={0.9}>
+      <AppCard
+        style={{
+          backgroundColor: colors.v2.teal,
+          height: 197,
+        }}
+      >
+        {!budget ? (
+          <Stack
+            style={{
+              paddingVertical: 16,
+              gap: 16,
+              alignItems: 'center',
+              justifyContent: 'center',
+              flex: 1,
+            }}
+          >
+            <MoneyIcon />
+            <ThemedText variant="header-sm" style={{ color: colors.v2.black, textAlign: 'center' }}>
+              Set Monthly Budget
+            </ThemedText>
+          </Stack>
+        ) : (
+          <Stack style={{ justifyContent: 'center', alignItems: 'center', height: '100%' }}>
+            <View style={{ marginBottom: 16 }}>
+              <PieChart current={remainingBudget} total={monthlyBudget} variant="sm" />
+            </View>
+            <View style={{ alignItems: 'center', gap: 8 }}>
+              <ThemedText variant="header-lg" color="inverted">
+                {toLocaleCurrencyFormat(remainingBudget)}
+              </ThemedText>
+              <ThemedText variant="title-sm" color="inverted">
+                Remaining Budget
+              </ThemedText>
+            </View>
+          </Stack>
+        )}
+      </AppCard>
+    </TouchableOpacity>
   );
 }
 
